818c71d2b1b94c0e2b1bea9ce089b86de9c4b020
lsp <lakhmanovsergey@gmail.com>
2016-02-11 01:24:48 +0300

    t

---
+++ src/test/java/ru/ru/lakhmanovsergey/CLI/CLIOptionsTest.java
@@ -0,0 +1,16 @@
+package ru.lakhmanovsergey.CLI;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lsp on 10.02.16.
+ */
+public class CLIOptionsTest {
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+}
