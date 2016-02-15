0e9c63570bd818ca5e43f78f208aaa8bc11ab274
lsp <lakhmanovsergey@gmail.com>
2016-02-16 00:39:37 +0300

    12.02

--- src/test/java/ru/lakhmanovsergey/CLI/CLIOptionsTest.java
+++ src/test/java/ru/lakhmanovsergey/CLI/CLIOptionsTest.java
@@ -1,82 +1,63 @@
 package ru.lakhmanovsergey.CLI;
 
+import com.sun.xml.internal.xsom.impl.util.SchemaTreeTraverser;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.apache.commons.cli.ParseException;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.*;
-
-public class CLIOptionsTest {
-/*    @Ignore
+@RunWith(DataProviderRunner.class)
+public class CLIOptionsTest extends Assert{
+    @DataProvider
+    public static Object[][] cliOptionsData(){
+        return new Object[][]{
+                {null,null,null},
+                {new String[] {"-h"},null,null},
+                {new String[] {"-f", "test"}, null,null},
+                {new String[] {"-d", "13-05-2014"}, new GregorianCalendar(2014,04,13 ),null},
+                {new String[] {"-d", "01-12-1538"}, new GregorianCalendar(1538,11,01 ),null},
+                {new String[] {"-d", "31-05-1971"}, new GregorianCalendar(1971,04,31 ),null},
+                {new String[] {"-d", "29-2-2016"}, new GregorianCalendar(2016,01,29 ),null},
+        };
+    }
+    @DataProvider
+    public static Object[][] cliOptionsExceptionData(){
+        return new Object[][]{
+                {new String[]{"-d","13-0"}},
+                {new String[]{"-d","test"}},
+                {new String[]{"-d","65736457654"}},
+                {new String[]{"-d","15-58-2015"}},
+                {new String[]{"-d","13.11.2018"}},
+                {new String[]{"-d","31-04-2014"}},
+                {new String[]{"-d","29-02-2013"}},
+        };
+    }
     @Test
-    public void testGetDate() throws ParseException {
-        SimpleDateFormat format=new SimpleDateFormat("dd-MM-yyyy");
-        CLIOptions.setCommandLine(new String[]{"-d", "13-05-2014"});
-        assertEquals("13-05-2014",format.format(CLIOptions.getDate().getTime()));
-        CLIOptions.setCommandLine(new String[]{"-d", "01-12-1538"});
-        assertEquals("01-12-1538",format.format(CLIOptions.getDate().getTime()));
-        CLIOptions.setCommandLine(new String[]{"-d", "31-05-1971"});
-        assertEquals("31-05-1971",format.format(CLIOptions.getDate().getTime()));
-        CLIOptions.setCommandLine(new String[]{"-d", "29-2-2016"});
-        assertEquals("29-02-2016",format.format(CLIOptions.getDate().getTime()));
-        CLIOptions.setCommandLine(null);
-        assertNull(CLIOptions.getDate());
-        CLIOptions.setCommandLine(new String[]{"-h"});
-        assertNull(CLIOptions.getDate());
-        CLIOptions.setCommandLine(new String[]{"-f","test"});
-        assertNull(CLIOptions.getDate());
+    @UseDataProvider("cliOptionsData")
+    public void getDataTest(String[] input, GregorianCalendar date, File file) throws ParseException {
+      CLIOptions.setCommandLine(input);
+        assertEquals(date,CLIOptions.getDate());
     }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx() throws ParseException {
-            CLIOptions.setCommandLine(new String[]{"-d", "13-0"});
-            CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx1() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "test"});
-        CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx2() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "54545454"});
-        CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx3() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "15-58-2015"});
-        CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx4() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "11.08.2013"});
-        CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx5() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "31-04-2013"});
-        CLIOptions.getDate();
-    }
-    @Ignore
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDateEx6() throws ParseException {
-        CLIOptions.setCommandLine(new String[]{"-d", "29-02-2013"});
-        CLIOptions.getDate();
-    }
-    */
     @Test
-    public void testGetFile() throws ParseException {
-        CLIOptions.setCommandLine(null);
-        assertNull(CLIOptions.getFile());
-        CLIOptions.setCommandLine(new String[]{"-f","/Users/lsp/IdeaProjects/CLIApp/pom.xml"});
-        assertEquals("/Users/lsp/IdeaProjects/CLIApp/pom.xml",CLIOptions.getFile().getAbsolutePath().toString());
+    @UseDataProvider("cliOptionsData")
+    public void getFileTest(String[] input,GregorianCalendar date,File file) throws ParseException {
+        CLIOptions.setCommandLine(input);
+        assertEquals(file,CLIOptions.getFile());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    @UseDataProvider("cliOptionsExceptionData")
+    public void testGetDataException(String[] input) throws ParseException {
+        CLIOptions.setCommandLine(input);
+        CLIOptions.getDate();
+        CLIOptions.getFile();
     }
 }
